eeff6b4200658df14f38dd8fc9e0ce407ab666ab
SHIVAM YADAV <shivamyadavskn@gmail.com>
2023-02-01 03:12:18 +0530

    initi

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import psycopg2
 
 
 app = Flask(__name__)
+
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = "1234"
 app.config['MYSQL_DB'] = "alchemy"
 mysql = MySQL(app)
-
+conn = psycopg2.connect("postgresql://root:m2kVAvjae5EnqQtUjSijowhGZYKB9VlZ@dpg-cfcnve02i3mhen7u9uqg-a.oregon-postgres.render.com/alchemy")
 
 
 @app.route('/')
 def dummy():
-    cur = mysql.connection.cursor()
+    cur=conn.cursor()
     cur.execute("SELECT*FROM demo")
-    account = cur.fetchall()
+    account=cur.fetchall()
     print(account)
     return render_template('home.html', account=account)
 
